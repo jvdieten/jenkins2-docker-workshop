aa7cc2de7fb51b6ea208b43c2274592acfad39ca
Joost van Dieten <joost@detesters.nl>
2016-12-08 20:51:23 +0100

    Update EmployeeSearchPage.java
    test

--- employees-app/src/test/src/test/java/com/example/employees/test/ui/EmployeeSearchPage.java
+++ employees-app/src/test/src/test/java/com/example/employees/test/ui/EmployeeSearchPage.java
@@ -3,5 +3,5 @@ package com.example.employees.test.ui;
 import net.serenitybdd.core.pages.PageObject;
 import net.thucydides.core.annotations.DefaultUrl;
 
-@DefaultUrl("http://localhost:9000")
+@DefaultUrl("http://fervent_jones:9000")
 public class EmployeeSearchPage extends PageObject {}
